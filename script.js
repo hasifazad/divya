0293bae81b7c314caf6bc2d348e67d740ad3cb73
hasif_azad <asifazad114@gmail.com>
2024-03-13 09:56:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// let num = [1, {
+//     name: 'john',
+//     age: 45,
+//     skills: ['css', 'html', 'java'],
+//     exp: {
+//         css: '1yr',
+//         html: '2yr'
+//     }
+// }, 'hello', [4, 78, 'g', [1, 4], 'h'], true, null, undefined, 'sdfds']
+
+// console.log(num[1].exp.css);
+
+let obj = {
+
+    name: 'john',
+    age: 45,
+    fun: function () {
+        console.log('hellooo');
+    }
+}
+
+obj.fun()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
